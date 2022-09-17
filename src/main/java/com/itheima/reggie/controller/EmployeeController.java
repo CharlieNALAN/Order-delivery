53cd7fd7c108edd3f4f5ae5931bcd297df84fa59
CharlieNAlan <wangjunmiao1209@163.com>
2022-09-17 10:56:19 +0800

    complete login function

--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -1,11 +1,17 @@
 package com.itheima.reggie.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Employee;
 import com.itheima.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @RestController
@@ -14,5 +20,31 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    /**
+     * 员工登录
+     * @param request
+     * @param employee
+     * @return R
+     */
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request,@RequestBody Employee employee){
+        String password = employee.getPassword();
+        password=DigestUtils.md5DigestAsHex(password.getBytes());
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername,employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
+        if(emp == null) {
+            return R.error("登陆失败");
+        }
+        if(!emp.getPassword().equals(password)){
+            return R.error("登陆失败");
+        }
+        if(emp.getStatus() == 0){
+            return R.error("登陆失败");
+        }
+        HttpSession session = request.getSession();
+        session.setAttribute("employee",emp.getId());
+        return R.success(emp);
+    }
 
 }

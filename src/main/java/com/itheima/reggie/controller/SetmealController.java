34714ee2874827fa20be9285c7e0598adca48670
CharlieNAlan <wangjunmiao1209@163.com>
2022-09-23 17:29:51 +0800

    complete the base of SetMeal

---
+++ src/main/java/com/itheima/reggie/controller/SetmealController.java
@@ -0,0 +1,26 @@
+package com.itheima.reggie.controller;
+
+import com.itheima.reggie.service.SetmealDishService;
+import com.itheima.reggie.service.SetmealService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 套餐管理
+ */
+@RestController
+@RequestMapping("/setmeal")
+@Slf4j
+public class SetmealController {
+
+    @Autowired
+    private SetmealService setmealService;
+
+    @Autowired
+    private SetmealDishService setmealDishService;
+
+
+}

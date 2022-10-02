480de82be589d49fb567ed483089c8c4d62d5cdc
CharlieNAlan <wangjunmiao1209@163.com>
2022-10-02 15:22:05 +0800

    complete order

--- src/main/java/com/itheima/reggie/service/OrderService.java
+++ src/main/java/com/itheima/reggie/service/OrderService.java
@@ -5,4 +5,9 @@ import com.itheima.reggie.entity.Orders;
 
 
 public interface OrderService extends IService<Orders> {
+    /**
+     * 用户下单
+     * @param orders
+     */
+    public void submit(Orders orders);
 }
